71dd03a078a0f56d8df9bd303be1261fcfe41a0d
charinrat bansong <charinrat_b@hotmail.com>
2020-11-19 17:00:46 +0800

    cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :doses, only: [:new, :create]
+  # /cocktails/:cocktail_id/doses/new  doses#new
+  # /cocktails/:cocktail_id/doses   doses#create
 end
+resources :doses, only: [:destroy]
+resources :ingredient
+end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Cocktail.destroy_all
+
+response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredients = JSON.parse(response)
+
+ingredients['drinks'].each do |drink|
+  ingredient = Ingredient.create(name: drink.values[0])
+  puts "#{ingredient.name}"
+end
+
+puts "COMPLETE"
